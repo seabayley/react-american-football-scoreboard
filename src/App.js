918f8a54be3b3c72e204aaafd24802f4f63ee672
Christopher Bailey <seabayley@outlook.com>
2019-09-09 08:50:24 -0500

    Added quarter/down/yards functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [currentDown, setCurrentDown] = useState(0);
+  const [currentQuarter, setCurrentQuarter] = useState(0);
+  const [currentYtg, setCurrentYtg] = useState(0);
+  const [currentYards, setCurrentYards] = useState(0);
 
   useEffect(() => {
     let timer = setInterval(() => {
@@ -19,12 +23,38 @@ function App() {
 
   function secondsToMinutes(seconds) {
     let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
     let secondsRemainder = Math.floor(seconds % 60);
     let minStr = minutes.toString().padStart(2, '0');
-    let hourStr = hours.toString().padStart(2, '0');
     let secRStr = secondsRemainder.toString().padStart(2, '0');
-    return (`${hourStr}:${minStr}:${secRStr}`);
+    return (`${minStr}:${secRStr}`);
+  }
+
+  function changeScore(team, amount) {
+    if (team === 'home') {
+      setHomeScore(homeScore + amount);
+    }
+    else if (team === 'away') {
+      setAwayScore(awayScore + amount);
+    }
+  }
+
+  function nextDown() {
+    if (currentDown === 4) {
+      setCurrentDown(1);
+    }
+    else {
+      setCurrentDown(currentDown + 1);
+    }
+
+  }
+
+  function nextQuarter() {
+    if (currentQuarter === 4) {
+      setCurrentQuarter(1);
+    }
+    else {
+      setCurrentQuarter(currentQuarter + 1);
+    }
   }
 
   return (
@@ -44,17 +74,23 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow props={[currentDown, currentQuarter, currentYards, currentYtg]} />
       </section>
       <section className="buttons">
+        <div className="boardButtons">
+          <button className="down_button" onClick={() => nextDown()}>Current Down</button>
+          <button className="down_button" onClick={() => setCurrentYtg(currentYtg + 1)}>Yards to Go</button>
+          <button className="down_button" onClick={() => setCurrentYards(currentYards + 1)}>Ball On</button>
+          <button className="down_button" onClick={() => nextQuarter()}>Quarter</button>
+        </div>
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore((homeScore + 7))}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore((homeScore + 3))}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => changeScore('home', 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => changeScore('home', 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore((awayScore + 7))}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore((awayScore + 3))}>Away Field Goal </button>
+          <button className="awayButtons__touchdown" onClick={() => changeScore('away', 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => changeScore('away', 3)}>Away Field Goal </button>
         </div>
       </section>
     </div>
